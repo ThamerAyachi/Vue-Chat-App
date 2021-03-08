f0bbf9eb42a98a979bb8abc17e74b209e53b2f32
ThamerAyachi <thamerayachi@Thamer.Thamer>
2021-03-08 20:23:33 +0100

    Createed the New Project

---
+++ app/src/components/Chat.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container-sm mt-20">
+    <div class="mx-5" v-if="isLogin">
+      <Message
+        v-for="{ id, text, userPhotoURL, userName, userId } in messages"
+        :key="id"
+        :name="userName"
+        :photo-url="userPhotoURL"
+        :sender="userId === user?.uid"
+      >
+        {{ text }}
+      </Message>
+    </div>
+    <div class="mx-5" v-else>
+      <h2 class="titre-ju">Join Us Now!</h2>
+    </div>
+  </div>
+
+  <div ref="bottom" class="mt-20" />
+
+  <div class="bottom">
+    <div class="container-sm">
+      <form v-if="isLogin" @submit.prevent="send">
+        <input v-model="message" placeholder="Message" required />
+        <button type="submit">
+          <SendIcon />
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, watch, nextTick } from 'vue'
+import { useAuth, useChat } from '@/firebase'
+
+import SendIcon from './SendIcon.vue'
+import Message from './Message.vue'
+
+export default {
+  components: { Message, SendIcon },
+  setup() {
+    const { user, isLogin } = useAuth()
+    const { messages, sendMessage } = useChat()
+
+    const bottom = ref(null)
+    watch(
+      messages,
+      () => {
+        nextTick(() => {
+          bottom.value?.scrollIntoView({ behavior: 'smooth' })
+        })
+      },
+      { deep: true }
+    )
+
+    const message = ref('')
+    const send = () => {
+      sendMessage(message.value)
+      message.value = ''
+    }
+
+    return { user, isLogin, messages, bottom, message, send }
+  }
+}
+</script>
